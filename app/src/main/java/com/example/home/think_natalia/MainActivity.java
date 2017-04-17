75ffc8afc085bf3d5855a771f5f4ce757666f997
mohamed ebrahim <ebrahimm131@gmil.com>
2017-04-17 16:55:23 +0200

    add Toast Hello World

--- app/src/main/java/com/example/home/think_natalia/MainActivity.java
+++ app/src/main/java/com/example/home/think_natalia/MainActivity.java
@@ -22,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        Toast.makeText(this, "Hello World", Toast.LENGTH_SHORT).show();
     }
 }
