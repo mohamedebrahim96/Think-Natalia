93a574a86c617bc9323fb123d528f2fd149bde24
mohamed ebrahim <ebrahimm131@gmil.com>
2017-04-29 00:47:06 +0200

    remove some libraries from gradle

--- app/src/main/java/com/example/home/think_natalia/MainActivity.java
+++ app/src/main/java/com/example/home/think_natalia/MainActivity.java
@@ -32,7 +32,6 @@ import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
